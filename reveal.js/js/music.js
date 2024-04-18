c254eb307d7a271ee4f9df9bb58fbde85aac82e1
GGarnik <garnik2828@gmail.com>
2024-04-18 17:22:31 +0200

    Сшиты все части
    Всё собрано воедино, добавлена музыка, переходы между слайдами и выбор в части Димы.

--- reveal.js/js/music.js
+++ reveal.js/js/music.js
@@ -1,8 +1,9 @@
-function createSound(url,{volume=0.5, timeUp=1}){
+function createSoundtrack(url,{volume=0.5, timeUp=1}){
     let audio = new Audio(); // Создание элемента аудие
     audio.src = url; // путь к самой мелодии
     audio.autoplay = true; //Автозапуск
     audio.volume = 0; //Громкость
+    audio.loop = true;
     const l = setInterval(()=>{
         audio.volume+=0.01*volume;
     },timeUp*10);
@@ -11,4 +12,12 @@ function createSound(url,{volume=0.5, timeUp=1}){
     },(1000*volume-10)*timeUp);
 }
 
+function createSound(url,{volume=0.5}){
+    let audio = new Audio(); // Создание элемента аудие
+    audio.src = url; // путь к самой мелодии
+    audio.autoplay = true; //Автозапуск
+    audio.volume = volume; //Громкость
+}
+
 // createSound("music/name.mp3", 0.5, 1);
+createSoundtrack("music/Vopna.mp3", 1);
